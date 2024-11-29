b41edfce47366f2c42828c1fb4689f5f68b1686e
sarahrainsberger <sarahrainsberger@gmail.com>
2024-11-29 18:28:10 +0000

    demo code ready for 5 (beta)

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,12 +2,12 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 
 export async function GET() {
-  const posts = await getCollection('posts');
+  const posts = await getCollection("blog");
   return rss({
     title: 'Astro Learner | Blog',
     description: 'My journey learning Astro',
     site: 'https://my-blog-site.netlify.app',
-    items: posts.map((post) => ({
+    items: posts.map((post:any) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
